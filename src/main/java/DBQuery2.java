daaf7c051f5914ca400e7999d8d837b2365e8653
Ruben Weitzman <rw4917@ic.ac.uk>
2019-11-28 10:41:04 +0000

    v1

--- src/main/java/DBQuery2.java
+++ src/main/java/DBQuery2.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class DBQuery2 {
     public static void main(String[] args) throws SQLException {
-        String dbUrl = "JDBC_DATABASE_URL";
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
         try
         {
             Class.forName("org.postgresql.Driver");

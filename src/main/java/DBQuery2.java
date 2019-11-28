46abac03fbca32177b472d870a303375fd24d165
Ruben Weitzman <rw4917@ic.ac.uk>
2019-11-28 10:32:36 +0000

    v1

---
+++ src/main/java/DBQuery2.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+public class DBQuery2 {
+    public static void main(String[] args) throws SQLException {
+        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+        try
+        {
+            Class.forName("org.postgresql.Driver");
+        } catch (Exception e) {
+        }
+        Connection conn= DriverManager.getConnection(dbUrl, "rubenweitzman","Ruben3001");
+
+        try {
+            Statement s=conn.createStatement();
+            String sqlStr2 = "create table patientofdoctor(\n" +
+                    "    id SERIAL PRIMARY KEY ,\n" +
+                    "    patientid int NOT NULL,\n" +
+                    "    doctorid int NOT NULL\n" +
+                    ");\n";
+            s.executeQuery(sqlStr2);
+            String sqlStr = "SELECT * FROM patients WHERE id>1;";
+            ResultSet rset=s.executeQuery(sqlStr);
+            while(rset.next()){
+                System.out.println(rset.getInt("id")+" "+ rset.getString("familyname")); }
+            rset.close();
+            s.close();
+            conn.close();
+        }
+        catch (Exception e){ }
+
+    }
+}

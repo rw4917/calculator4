858aa291e19392fd4a4518d57a9417cb376ab790
Ruben Weitzman <rw4917@ic.ac.uk>
2019-12-05 11:53:27 +0000

    v1

--- src/main/java/conn.java
+++ src/main/java/conn.java
@@ -21,13 +21,8 @@ public class conn {
     }
 
     private static Connection getConnection() throws URISyntaxException, SQLException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
-
-        return DriverManager.getConnection(dbUrl, username, password);
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        return DriverManager.getConnection(dbUrl);
     }
 
 }

cd484fade47178fe258773fb6774dacb1c89a73b
Ruben Weitzman <rw4917@ic.ac.uk>
2019-12-11 16:53:15 +0000

    v1

--- src/main/java/CreateDB.java
+++ src/main/java/CreateDB.java
@@ -17,19 +17,16 @@ public class CreateDB {
                     "    patientid int NOT NULL," +
                     "    doctorid int NOT NULL" +
                     ");\n";
-            s.executeQuery(sqlStr2);
+            s.executeUpdate(sqlStr2);
             String sqlStr3 = "create table patients (\n" +
                     "                    id SERIAL PRIMARY KEY,\n" +
                     "                    familyname varchar(128) NOT NULL, givenname varchar(128) NOT NULL, phonenumber varchar(32)\n" +
                     "            );";
-            s.executeQuery(sqlStr3);
+            s.executeUpdate(sqlStr3);
 
             System.out.println("Hopefully we created the table ");
             String sqlStr = "insert into patients (familyname,givenname,phonenumber) values('Jones','Bill','07755678899');";
-            ResultSet rset=s.executeQuery(sqlStr);
-            while(rset.next()){
-                System.out.println(rset.getInt("id")+" "+ rset.getString("familyname")); }
-            rset.close();
+            s.executeUpdate(sqlStr);
             s.close();
             connection.close();
         }

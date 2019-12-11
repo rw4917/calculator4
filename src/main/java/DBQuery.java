8b98dc88629af0793aa5560a779afab0aee3818e
Ruben Weitzman <rw4917@ic.ac.uk>
2019-12-11 19:17:07 +0000

    v1

--- src/main/java/DBQuery.java
+++ src/main/java/DBQuery.java
@@ -1,34 +1,52 @@
 import java.sql.*;
+import java.net.URISyntaxException;
+
 
 public class DBQuery {
     public String output="";
-
-    public void main(String[] args) throws SQLException
-    {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        try
-        {
-            Class.forName("org.postgresql.Driver");
-        } catch (Exception e) {
-        }
-        Connection conn= DriverManager.getConnection(dbUrl);
-
+    public DBQuery() {
+        System.out.println("Something is working");
         try {
-            Statement s=conn.createStatement();
+            Connection connection = getConnection();
+            Statement s=connection.createStatement();
+            System.out.println("connection works");
+
+//            String sqlStr3 = "create table patients (\n" +
+//                    "                    id SERIAL PRIMARY KEY,\n" +
+//                    "                    familyname varchar(128) NOT NULL, givenname varchar(128) NOT NULL, phonenumber varchar(32)\n" +
+//                    "            );";
+//            s.executeUpdate(sqlStr3);
+//            String sqlStr2 = "create table patientofdoctor(" +
+//                    "    id SERIAL PRIMARY KEY ," +
+//                    "    patientid int NOT NULL," +
+//                    "    doctorid int NOT NULL" +
+//                    ");\n";
+//            s.executeUpdate(sqlStr2);
+//
             String sqlStr = "SELECT * FROM patients";
             ResultSet rset=s.executeQuery(sqlStr);
             while(rset.next()){
                 output+=rset.getInt("id")+" "+ rset.getString("familyname"); }
             rset.close();
             s.close();
-            conn.close();
         }
-        catch (Exception e){ }
+        catch (Exception e)
+        {
+            System.out.println("There was a problem:");
+            e.printStackTrace();
+        }
 
 
     }
     public String getOutput()
     {
+        System.out.println("Is this a string");
         return output;
+
+    }
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        System.out.println("Working until now");
+        return DriverManager.getConnection(dbUrl);
     }
 }

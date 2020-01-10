d5b0e881077198a36dcdba524c3ce96d0762884e
Ruben Weitzman <rw4917@ic.ac.uk>
2020-01-10 16:16:33 +0100

    v1

--- src/main/java/BloodGLucoseLevel.java
+++ src/main/java/BloodGLucoseLevel.java
@@ -1,14 +1,29 @@
 
 
 import java.io.Serializable;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Date;
+import java.sql.*;
+
+
+
+
 
 public class BloodGLucoseLevel implements Serializable {
     private String date;
     private double level ;
-    public BloodGLucoseLevel()
+    public BloodGLucoseLevel() throws URISyntaxException, SQLException
     {
-        level =0;
+        level = 0;
+        Connection connection = getConnection();
+        Statement s = connection.createStatement();
+        System.out.println("connection works");
+        s.executeUpdate("insert into logs (patients,method,glucoselevel) values('0','1','0.2');");
+
     }
     public double getLevel()
     {
@@ -23,4 +38,11 @@ public class BloodGLucoseLevel implements Serializable {
         this.date = new Date().toString();
 
     }
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        System.out.println("Working until nnow");
+        return DriverManager.getConnection(dbUrl);
+    }
 }
+

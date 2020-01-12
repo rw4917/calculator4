e07a89e807a2e67256093fc48a256bc30ac5c8ed
Ruben Weitzman <rw4917@ic.ac.uk>
2020-01-12 20:20:23 +0000

    v1

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -6,12 +6,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns={"/patients"},loadOnStartup = 1)
 public class MyServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
 
 
@@ -22,20 +27,40 @@ public class MyServlet extends HttpServlet {
         resp.getWriter().write("Hello world");
 
     }
-    @Override
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        System.out.println("In do post");
-        String reqBody=req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 
-        System.out.println("Json is "+reqBody);
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("In do post");
+        String reqBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
+
+        System.out.println("Json is " + reqBody);
         Gson gson = new Gson();
-        RegistrationDetails reg=gson.fromJson(reqBody,RegistrationDetails.class);
+        RegistrationDetails reg = gson.fromJson(reqBody, RegistrationDetails.class);
         String str2 = String.valueOf(reg.getEmail());
 
         resp.setContentType("text/html");
         resp.getWriter().write(str2);
         resp.getWriter().write("Thank you client!");
+        try {
+            Connection connection = getConnection();
+            Statement s = connection.createStatement();
+            System.out.println("connection works");
+            s.executeUpdate("insert into patients (familyname,givenname,phonenumber,username,password) values('Jones','Bill','07755678899','ruben','weitz');");
+
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
     }
 
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        System.out.println("Working until nnow");
+        return DriverManager.getConnection(dbUrl);
+    }
 }
